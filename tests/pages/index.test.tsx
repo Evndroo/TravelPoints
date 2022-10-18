b2c4a88a7f7fb39a825b4036e3d58a51f405e1f5
evndroo <evandro.slipjr@gmail.com>
2022-10-18 19:58:48 -0300

    test: add new Home Page content and style

--- tests/pages/index.test.tsx
+++ tests/pages/index.test.tsx
@@ -1,9 +1,11 @@
 import { render, screen } from '@testing-library/react'
 import Home from '@/pages'
 
-describe('Teste', () => {
-	it('teste', () => {
+describe('<Home/>', () => {
+	it('should show correct heading text', () => {
 		render(<Home />)
-		expect(screen.getByText('Hello World')).toBeInTheDocument()
+		expect(
+			screen.getByRole('heading', { name: /React Avançado/i })
+		).toBeInTheDocument()
 	})
 })
