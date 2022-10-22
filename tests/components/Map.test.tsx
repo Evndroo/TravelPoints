012c4d3dce0f92b6d246abdb586f39b7a5e99d7c
evndroo <evandro.slipjr@gmail.com>
2022-10-22 11:30:49 -0300

    test: make map markers dynamic based on prop

--- tests/components/Map.test.tsx
+++ tests/components/Map.test.tsx
@@ -1,13 +1,42 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 
 import { Map } from '@/components'
 
 describe('<Map/>', () => {
 	it('should render without any marker', () => {
 		render(<Map />)
+
+		expect(
+			screen.getByRole('link', {
+				name: /a js library for interactive maps/i
+			})
+		).toBeInTheDocument()
 	})
 
 	it('should render with the marker in correct place', () => {
-		render(<Map />)
+		const place = {
+			id: '1',
+			name: 'Petrópolis',
+			slug: 'pretropolis',
+			location: {
+				latitude: 0,
+				longitude: 100
+			}
+		}
+
+		const secondPlace = {
+			id: '2',
+			name: 'Reykjavik',
+			slug: 'Reykjavik',
+			location: {
+				latitude: 799,
+				longitude: -897
+			}
+		}
+
+		render(<Map places={[place, secondPlace]} />)
+
+		expect(screen.getByTitle(/petrópolis/i)).toBeInTheDocument()
+		expect(screen.getByTitle(/reykjavik/i)).toBeInTheDocument()
 	})
 })
